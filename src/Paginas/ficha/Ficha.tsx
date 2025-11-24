1bfcd306483cac22eb5b3b43f5d218805158218b
dminoru13 <dminoru13@gmail.com>
2025-11-24 14:42:26 -0300

    ta dando tudo errado

--- src/Paginas/ficha/Ficha.tsx
+++ src/Paginas/ficha/Ficha.tsx
@@ -1,50 +1,51 @@
   import { useState } from 'react'
   import viteLogo from '/vite.svg'
-  //import './Ficha.css'
+  import './Ficha.css'
 
 
-  type Modulos = {
-    modulo: string[];
-    numero: number;
+  type Modulo = {
+    id: string;
+    conteudo: string;
   };
 
-
-
-  export function Repetidor({modulo, numero}: Modulos){
-    return(
-      <div>
-        {modulo.map((modulo, i) => (
-          <div className='modulo'>
-            <p key={i}>{modulo}</p>
-            <input type="number" value = {i}/>
-            <p>{numero}</p>
-          </div>
-          
-        ))}
-      </div>
-    )
+  interface RepetidorModulos {
+    modulos: Modulo[];
   }
 
 
+
+
+
+
   function Ficha() {
+    const [nomeModulo, setnomeModulo] = useState('')
+    const [conteudoModulo, setConteudoModulo] = useState('')
 
-    const [pagina, setPagina] = useState([''])
-    const [valorInput, setValorInput] = useState('')
+    const [modulos, setModulos] = useState<Modulo[]>([
+      {id: "1", conteudo: ""},
+      {id: "2", conteudo: ""}
+    ])
     
-    function adicionar() {
-      setPagina(prev => [...prev, valorInput])
-    }
-
     return (
-      <>
         <div>
-          <button onClick={adicionar}>MAIS amor</button>
 
-          <input type="text" value={valorInput} onChange={e => setValorInput(e.target.value)}/>
+          <input type="text" value={nomeModulo} onChange={e => setnomeModulo(e.target.value)}/>
+          <p>conteudo:</p>
+          <input type="text" value={conteudoModulo} onChange={e => setConteudoModulo(e.target.value)}/>
 
-          <Repetidor modulo = {pagina} numero = {1}/>
+          <div className='conteiner'>
+          {modulos.map((modulo) => (
+            <div className='id' key={modulo.id}>
+              <p>{modulo.id}</p>
+              <p>{modulo.conteudo}</p>
+              <button>atualizar conteudo</button>
+
+              
+            </div>
+            
+          ))}
         </div>
-      </>
+
     )
   }
 
