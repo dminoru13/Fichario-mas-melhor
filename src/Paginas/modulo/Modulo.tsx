1e1ba195bbd129d21630938abb2599bf8644028d
notridani <musicodorpg@gmail.com>
2025-12-03 12:39:53 -0300

    arrumei um trocinho

--- src/Paginas/modulo/Modulo.tsx
+++ src/Paginas/modulo/Modulo.tsx
@@ -7,43 +7,43 @@
 
   
   
-function Modulo({ id }: { id: string }) {
+  function Modulo({ id }: { id: string }) {
 
-  const { modulos, adicionarModulo } = useModulo();
+      const { modulos, adicionarModulo } = useModulo();
 
-  function encontrar(id: string, lista: TipoModulo[]): TipoModulo | null {
-    for (const item of lista) {
-      if (item.id === id) return item;
-      const f = encontrar(id, item.ModuloFilho);
-      if (f) return f;
-    }
-    return null;
+      function encontrar(id: string, lista: TipoModulo[]): TipoModulo | null {
+        for (const item of lista) {
+          if (item.id === id) return item;
+          const f = encontrar(id, item.ModuloFilho);
+          if (f) return f;
+        }
+        return null;
+      }
+
+      const moduloAtual = encontrar(id, modulos);
+      if (!moduloAtual) return null;
+
+      function adicionarFilho() {
+        const numero = moduloAtual!.ModuloFilho.length + 1;
+        adicionarModulo(id, { id: id + "." + numero, ModuloFilho: [] });
+      }
+
+      return (
+        <div className="modulo">
+          <div className="cabecario">
+            <p>{id}</p>
+            <button onClick={adicionarFilho} className='btnAdd'>+</button>
+          </div>
+
+          <div className="filhos">
+            {moduloAtual.ModuloFilho.map(filho =>
+              <Modulo key={filho.id} id={filho.id} />
+            )}
+          </div>
+        </div>
+      );
   }
-
-  const moduloAtual = encontrar(id, modulos);
-  if (!moduloAtual) return null;
-
-  function adicionarFilho() {
-    const numero = moduloAtual!.ModuloFilho.length + 1;
-    adicionarModulo(id, { id: id + "." + numero, ModuloFilho: [] });
-  }
-
-  return (
-    <div className="modulo">
-      <div className="cabecario">
-        <p>{id}</p>
-        <button onClick={adicionarFilho} className='btnAdd'>+</button>
-      </div>
-
-      <div className="filhos">
-        {moduloAtual.ModuloFilho.map(filho =>
-          <Modulo key={filho.id} id={filho.id} />
-        )}
-      </div>
-    </div>
-  );
-}
 
 
   
-  export default Modulo
+export default Modulo
